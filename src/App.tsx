853246c3827a3bf58701662b6829680d289b147a
volant97 <volant97@naver.com>
2023-12-14 00:06:46 +0900

    [SETTING] 글로벌스타일 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <h1>세팅 끝!</h1>
     </>
   );
 }
 
 export default App;
-
-const GlobalStyle = createGlobalStyle`
-${reset}
-* {
-	box-sizing: border-box;
-}
-`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+* {
+	box-sizing: border-box;
+}
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
