31a316550d16fa6c4d86275b40afc27a0b87c778
volant97 <volant97@naver.com>
2023-12-14 00:00:54 +0900

    [SETTING] 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <h1>세팅 끝!</h1>
+    </>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+* {
+	box-sizing: border-box;
+}
+`;
